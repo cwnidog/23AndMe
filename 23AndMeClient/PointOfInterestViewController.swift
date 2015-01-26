300d50783d4fbe903ab507d2a4f62b89e34d0a86
John Leonard <cwnidog1@gmail.com>
2015-01-26 11:26:32 -0800

    Changing views due to limited API availability

--- 23AndMeClient/PointOfInterestViewController.swift
+++ 23AndMeClient/PointOfInterestViewController.swift
@@ -1,14 +1,14 @@
 //
-//  RisksViewController.swift
+//  PointOfInterestViewController.swift
 //  23AndMeClient
 //
-//  Created by John Leonard on 1/25/15.
+//  Created by John Leonard on 1/26/15.
 //  Copyright (c) 2015 John Leonard. All rights reserved.
 //
 
 import UIKit
 
-class RisksViewController: UIViewController {
+class PointOfInterestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
