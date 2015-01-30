14c770b0ab4c543022e41394fd3872abf7407050
asketola <star7anne@yahoo.com>
2015-01-29 20:04:32 -0800

    fixed plist to update content

--- CelebrityInterestViewController.swift
+++ CelebrityInterestViewController.swift
@@ -21,6 +21,8 @@ class CelebrityInterestViewController: UIViewController {
   var femaleName : String?
   var maleImageUrl : String?
   var femaleImageUrl : String?
+  var finalCountry : String?
+  var countryDictionary = [String:AnyObject]()
   
 //  var celebCountry : CelebrityNameDictionaryDemo  // calls the celebArray content array I made
 
@@ -39,44 +41,39 @@ class CelebrityInterestViewController: UIViewController {
       let path = NSBundle.mainBundle().pathForResource("CelebPropertyList", ofType: "plist") // creates a path to get the plist
       let celebDictionary = NSDictionary(contentsOfFile: path!) // creates the dictionary out of the plist
       
+      // println("CelebDictionary = \(celebDictionary)")
+      
       // loop through each country dictionary
-      /*
-      self.countryDescriptionLabel.text = "Celebrities with \(country.country) Ancestory"
+      
+//      if country!.country != nil {
+//        let finalCountry = country.country
+//      } else if Region.region != nil {
+//        let finalCountry = region.region
+//      } else if subRegion.subRegion != nil {
+//        let finalCountry = subRegion.subRegion
+//      }
+      
+      var finalCountry = "Unassigned"
+  
+      self.countryDescriptionLabel.text = "Celebrities with \(finalCountry) Ancestory"
 
-      let countryDictionary = celebDictionary![country.country] as [String : AnyObject]
+      var countryDictionary = celebDictionary![finalCountry] as [String : AnyObject]
+      println("countryDictionary = \(countryDictionary)")
       self.celebrityMaleLabel.text = countryDictionary["male"] as? String
       self.celebrityFemaleLabel.text = countryDictionary["female"] as? String
-*/
+      self.maleImageUrl = countryDictionary["maleImage"] as? String
+      self.femaleImageUrl = countryDictionary["femaleImage"] as? String
+      println("maleImageUrl = \(maleImageUrl)")
+      
+      requestImage()
+      
   }
-    
-//      var maleName = celebDictionary?.valueForKey("male") as? String
-//      var femaleName = celebDictionary?.valueForKey("female") as? String
-//      var maleImageUrl = celebDictionary?.valueForKey("maleImage") as? String
-//      var femaleImageUrl = celebDictionary?.valueForKey("femaleImage") as? String
-
-      
-      
-      
-      
-      // if the country name passed from the previous view controller matches the CelebDicitonary Key for country, then put on labels and images
-//      For celebCountry in celebDicitonary {
-//      if country == celebCountry {
-//        
-//        self.celebrityMaleLabel.text = self.celebDicionaty.maleName
-//        self.celebrityFemaleLabel.text = self.celebDicionary.femalName
-      
-
-        
-        
-//         self.celebrityFemaleImage.image =
-//         self.celebrityMaleImage.image =
-        
-        
- 
-      // put this into networkController?????
-      
-      func didRequestImage(results: NSDictionary) {
+  
+  
+      // func didRequestImage(results: NSDictionary) {
+      func requestImage() {
         var male_url = self.maleImageUrl
+        println("male_url = \(male_url)")
         var female_url = self.femaleImageUrl
         var mUrl = NSURL(string: male_url!)
         var fUrl = NSURL(string: female_url!)
@@ -93,6 +90,9 @@ class CelebrityInterestViewController: UIViewController {
             femaleImage = UIImage(data: data)
             self.celebrityFemaleImage.image = femaleImage
         })
+        // self.collectionView.reloadData()
+       // UIImageView.image.reloadData()
+
       }
 //    override func didReceiveMemoryWarning() {
 //        super.didReceiveMemoryWarning()
