b2f7b21217b76a02d75cf64917e340252c6a9220
Anne <star7anne@yahoo.com>
2015-02-18 14:25:19 -0800

    weird other files were some how not included in the original commit

---
+++ MaternalLineViewController.swift
@@ -0,0 +1,63 @@
+//
+//  MaternalLineViewController.swift
+//  23AndMeClient
+//
+//  Created by Josh Kahl on 1/29/15.
+//  Copyright (c) 2015 John Leonard. All rights reserved.
+//
+
+import UIKit
+
+class MaternalLineViewController: UIViewController, UIWebViewDelegate
+{
+  
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+  
+  @IBOutlet weak var webView: UIWebView!
+  
+  var maternalWebURL : String?
+  
+
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    self.activityIndicator.hidden = false
+  }
+  
+  override func viewDidAppear(animated: Bool)
+  {
+    if (NetworkController.sharedNetworkController.maternalHaplogroup == nil)
+    { // fetch info
+      NetworkController.sharedNetworkController.fetchUserHaplogroup( { (maternalHaplo, paternalHaplo, errorString) -> (Void) in
+        if(errorString == nil)
+        {
+          if(maternalHaplo != nil)
+          {
+            self.maternalWebURL = "https://www.23andme.com/you/haplogroup/maternal/?viewgroup=\(maternalHaplo!)&tab=story"
+            NetworkController.sharedNetworkController.maternalHaplogroup = maternalHaplo
+          }
+          NetworkController.sharedNetworkController.paternalHaplogroup = maternalHaplo
+        }
+        if(self.maternalWebURL != nil)
+        {
+          let request = NSURLRequest(URL: NSURL(string: self.maternalWebURL!)!)
+          self.activityIndicator.hidden = true
+          self.webView.loadRequest(request)
+        }
+      })
+    } else { // this will fire when the webView has been loaded previously.
+      let maternal = NetworkController.sharedNetworkController.maternalHaplogroup
+      self.maternalWebURL = "https://www.23andme.com/you/haplogroup/maternal/?viewgroup=\(maternal!)&tab=story"
+      let request = NSURLRequest(URL: NSURL(string: self.maternalWebURL!)!)
+      self.activityIndicator.hidden = true
+      self.webView.loadRequest(request)
+    }
+  }
+  
+
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+  }
+
+}
