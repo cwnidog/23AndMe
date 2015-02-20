e86309861c6df174acb8548c4f2f0c55c5097bbe
Josh Kahl <j7kahl@gmail.com>
2015-02-19 16:08:16 -0800

    "removed janky image at bottom of mainTableView"

--- 23AndMeClient/MenuTableViewController.swift
+++ 23AndMeClient/MenuTableViewController.swift
@@ -16,7 +16,9 @@ class MenuTableViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.accentImage.image = UIImage(named: "gel0.jpg")
+//    self.accentImage.image = UIImage(named: "gel0.jpg")
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
     
     if (newUser)
     {
@@ -30,61 +32,6 @@ class MenuTableViewController: UITableViewController {
     super.viewDidAppear(animated)
     self.navigationController?.delegate = nil // make sure that here are no zombies
     
-    if (NetworkController.sharedNetworkController.accessToken == nil || NetworkController.sharedNetworkController.needRefresh)
-    {
-      if NetworkController.sharedNetworkController.accessToken == nil
-      {
-        println("Access token is nil, setting up the web controller to ask for an initial token")
-      } // access token == nil
-        
-      else
-      {
-        println("Access token has timed out, setting up the web controller to ask for a new token")
-      }
-      let webVC = WebViewController()
-      self.presentViewController(webVC, animated: true, completion: { () -> Void in
-      }) // enclosure
-    } // if accessToken == nil
-    else
-    {
-      // we need to get the profile IDs
-      NetworkController.sharedNetworkController.fetchProfileID((), callback: { (profiles, userID, errorDescription) -> (Void) in
-        NetworkController.sharedNetworkController.profiles = profiles!
-        NetworkController.sharedNetworkController.userID = userID!
-      }) // fetchProfileID enclosure
-    }
-
-    
-    /*
-    if (NetworkController.sharedNetworkController.accessToken == nil) //new user -> onboard
-    {
-
-    }
-    else if (NetworkController.sharedNetworkController.accessToken == nil || NetworkController.sharedNetworkController.needRefresh)
-    {
-      if NetworkController.sharedNetworkController.accessToken == nil
-      {
-        println("Access token is nil, setting up the web controller to ask for an initial token")
-      } // access token == nil
-        
-      else
-      {
-        println("Access token has timed out, setting up the web controller to ask for a new token")
-      }
-      let webVC = WebViewController()
-      self.presentViewController(webVC, animated: true, completion: { () -> Void in
-      }) // enclosure
-    } // if accessToken == nil
-    else
-    {
-      // we need to get the profile IDs
-      NetworkController.sharedNetworkController.fetchProfileID((), callback: { (profiles, userID, errorDescription) -> (Void) in
-        NetworkController.sharedNetworkController.profiles = profiles!
-        NetworkController.sharedNetworkController.userID = userID!
-      }) // fetchProfileID enclosure
-    }*/
-  
-    /*
     // if we don't have a stored access token we need to ask for one
     if (NetworkController.sharedNetworkController.accessToken == nil || NetworkController.sharedNetworkController.needRefresh)
     {
@@ -108,6 +55,6 @@ class MenuTableViewController: UITableViewController {
         NetworkController.sharedNetworkController.profiles = profiles!
         NetworkController.sharedNetworkController.userID = userID!
       }) // fetchProfileID enclosure
-    }*/
+    }
   } //viewDidAppear()
  } // MenuTableViewController
